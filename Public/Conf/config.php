f2e4e667c46c7b4179c5065289e9812d288b4aa3
shrikl <553521882@qq.com>
2016-12-21 09:54:30 +0800

    使用线上数据库

--- Public/Conf/config.php
+++ Public/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'SHOW_PAGE_TRACE' => true,
-	'DB_DSN' => 'mysql://root:gwl106657qq@localhost:3306/thinkphp',
+	'DB_DSN' => 'mysql://db4lucky:dbforlucky@118.89.36.192:3306/lucky',
 	'DB_PREFIX' => 'tp_',
 	'TMPL_L_DELIM' => '<{',
 	'TMPL_R_DELIM' => '}>',
